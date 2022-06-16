77a38e0da1c9be830daca409d391f182551460fc
root <axel.mercier2@gmail.com>
2022-06-16 12:57:59 +0000

    FEATURE ADDED
    A function to translate each sequence from a fasta file to a csv file with, according to the six reading frames.
    
    Developped to use threads.

--- dna_function.py
+++ dna_function.py
@@ -1,6 +1,8 @@
 from Bio.Seq import Seq
 import threading
 
+
+csv_amino_acid_semaphore = threading.Semaphore()
 Debug = False
 
 def printif(text, debug = Debug):
@@ -33,10 +35,6 @@ def seq_to_amin(seq, dico_to_save, key):
     dico_to_save
     dico_to_save[key] = Seq(seq).translate() 
     return dico_to_save[key]
-
-
-
-
 
 
 def dna_to_amino_acid(sequence,titre):
@@ -72,6 +70,9 @@ def dna_to_amino_acid(sequence,titre):
 
     for i in [0,1,2]:
         # The following instructions use the threading module
+        #
+        # The length of the sequence are checked to be a multiple of 3, we cut the sequence to avoid exception.
+        #
         # we prepare the translation of the i th frame for the starting sequence
         t1 = threading.Thread(target=seq_to_amin, args=(sequence[i:length_seq-(length_seq-i)%3], the_six_reads, titre+"+"+str(i), ))
         t1.start()
@@ -89,10 +90,78 @@ def dna_to_amino_acid(sequence,titre):
     return the_six_reads
 
 
+def Fasta_to_amino_acid(fasta_file_name):
+    """
+    Create a csv file with the translated sequence from a fasta file
+
+    The csv file is a database with the main key which is an integer.
+
+    
+
+    """
+    list_threads = []
+    with open(fasta_file_name, 'r', encoding = 'utf-8') as fIn:
+        # we set the index for the database 
+        index = 0 
+        line  = fIn.readline()
+        while(len(line) != 0):
+            titre = line[:-1] 
+            sequence = fIn.readline()[:-1]
+           
+            # we set the thread 
+            thread_current = threading.Thread(target=write_fasta_to_amino_acid, args=(sequence, titre, fasta_file_name, index,) )
+            thread_current.start()
+            list_threads.append(thread_current)
+            
+            # we read the next line
+            line = fIn.readline()
+            # we increase the index of 6 
+            index += 6
+        
+        # we wait each thread
+        for t in list_threads:
+            t.join()
+
+    return index
+            
+
+def write_fasta_to_amino_acid(sequence, titre, fasta_file_name, index):
+    """
+    This function writes the six sequences of amino acid after the translation of the dna sequence.
+
+    
+
+
+    """
+    translated_sequences_six_reading_frame = dna_to_amino_acid(sequence, titre)
+
+    csv_amino_acid_semaphore.acquire() 
+    with open(fasta_file_name+"_Translated_sequence.csv", 'a', encoding = 'utf-8') as fOut:
+        for key in translated_sequences_six_reading_frame.keys():
+            fOut.write(str(index)+" , "+str(key)+" , "+str(translated_sequences_six_reading_frame[key])+"\n")
+            index+=1
+    csv_amino_acid_semaphore.release()
+
+    return index 
+
+
+
+
 
 if __name__ == '__main__':
     import doctest
     # to test the different functions 
     doctest.testmod()
 
+    # to test
+    import os 
+    os.system("touch fasta_test.fasta")
+    with open("fasta_test.fasta",'a', encoding = 'utf-8') as f:
+        f.write("> seq1\n")
+        f.write("ATCGATCGATCGATCGATCG\n")
+        f.write("> seq2\n")
+        f.write("TCGATCAGTCAGCTATGCTAGCT\n")
+    Fasta_to_amino_acid("fasta_test.fasta")
+    
+    os.system("rm fasta_test.fasta")
 
