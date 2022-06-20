2d9e95a1ff58f98cb2fb8975863aadac3acd8b8a
root <axel.mercier2@gmail.com>
2022-06-20 11:32:48 +0000

    Main Started

---
+++ main.py
@@ -0,0 +1,36 @@
+import argparse
+import mass_spectrom_function
+import dna_function.py
+
+
+
+
+
+# the file with the mass data
+spectra_file_name
+
+# the file with the sequence after sequencing
+fasta_seq_file
+
+# the fasta file which contains protein sequence
+pdb_fasta_file 
+
+
+
+
+# We validate the msms with the sequencing data 
+# We search the matchs between ms ms data and the fasta data
+# we saved it in a csv file to use the data 
+
+
+
+# we search the known protein from msms data and protein data base fasta
+
+
+
+
+
+
+
+
+
